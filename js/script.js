f3eb576743ebad0a3c63e627e0d8e6c58073a7ad
= <=>
2022-06-18 00:36:34 -0500

    add shop category

--- js/script.js
+++ js/script.js
@@ -19,6 +19,63 @@ window.onscroll = () =>{
   }
 }
 
+$('#customers-testimonials').owlCarousel({
+  /* rtl: true, */
+  loop: true,
+  nav: true,
+  navText: ["<div class='nav-button owl-prev'>‹</div>", "<div class='nav-button owl-next'>›</div>"],
+  /* center: true, */
+  items: 4,
+  margin: 0,
+  autoplay: false,
+  dots: false,
+  autoplayTimeout: 8500,
+  smartSpeed: 450,
+
+  responsive: {
+    0: {
+      items: 1
+    },
+    768: {
+      items: 2
+    },
+    1170: {
+      items: 4
+    },
+    3840: {
+      items: 4
+    }
+  }
+});
+
+$('.nice-scroll .btn').click(function(){
+
+  let filter = $(this).attr('data-filter');
+  if(filter == 'all'){
+      console.log('prueba1');
+      $('.product__item .box').show(400);
+  }else{
+      console.log('prueba2');
+      $('.product__item .box').not('.'+filter).hide(200);
+      $('.product__item .box').filter('.'+filter).show(400);
+  }
+
+  $(this).addClass('button-active').siblings().removeClass('button-active');
+
+});
+
+$('.owl-filter-class').on('click', '.item', function () {
+  var $item = $(this);
+  var filter = $item.data('owl-filter')
+  owl.owlcarousel2_filter(filter);
+});
+var owl = $('.owl-carousel-class').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  items: 4 ,
+});
+
 
 popupWhatsApp = () => {
   
